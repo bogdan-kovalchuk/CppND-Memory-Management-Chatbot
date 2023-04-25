a455b7bf15e22ca544af8f9f6588f6969086a1e8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2023-04-25 10:00:00 +0300

    Guard ChatBot::SetCurrentNode against nodes with no answers

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -160,6 +160,9 @@ void ChatBot::SetCurrentNode(GraphNode *node)
 
     // select a random node answer (if several answers should exist)
     std::vector<std::string> answers = _currentNode->GetAnswers();
+    if (answers.empty())
+        return; // a node with no answers has nothing to send to the user
+
     std::mt19937 generator(int(std::time(0)));
     std::uniform_int_distribution<int> dis(0, answers.size() - 1);
     std::string answer = answers.at(dis(generator));
